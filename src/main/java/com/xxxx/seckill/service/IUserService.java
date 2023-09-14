cab22c32a6e8af82d54794bdc91a4bf3d5cd2fd2
yanshanbei <1143219505@qq.com>
2023-09-14 18:57:48 +0800

    实现mobile校验, dologin
    实现了点击登录,校验信息的功能

---
+++ src/main/java/com/xxxx/seckill/service/IUserService.java
@@ -0,0 +1,28 @@
+package com.xxxx.seckill.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.seckill.pojo.User;
+import com.xxxx.seckill.vo.LoginVo;
+import com.xxxx.seckill.vo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Mr.Shi
+ * @since 2023-09-03
+ */
+public interface IUserService extends IService<User> {//业务逻辑都写在service层
+
+    /**
+     * 登陆
+     * @param loginVo
+     * @return
+     */
+    RespBean doLogin(LoginVo loginVo);//接口 再找实现类
+
+}
---
+++ src/main/java/com/xxxx/seckill/service/impl/UserServiceImpl.java
@@ -0,0 +1,64 @@
+package com.xxxx.seckill.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xxxx.seckill.mapper.UserMapper;
+import com.xxxx.seckill.pojo.User;
+import com.xxxx.seckill.service.IUserService;
+import com.xxxx.seckill.utils.MD5Util;
+import com.xxxx.seckill.utils.ValidatorUtil;
+import com.xxxx.seckill.vo.LoginVo;
+import com.xxxx.seckill.vo.RespBean;
+import com.xxxx.seckill.vo.RespBeanEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Mr.Shi
+ * @since 2023-09-03
+ */
+@Service//业务逻辑的具体实现
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+    /**
+     * 登录
+     * @param loginVo
+     * @since 2023-09-03
+     */
+    @Override
+    public RespBean doLogin(LoginVo loginVo) {
+        String mobile = loginVo.getMobile();
+        String password = loginVo.getPassword();
+
+        if (StringUtils.isEmpty(mobile) || StringUtils.isEmpty(password)){
+            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
+        }
+        if (!ValidatorUtil.isMobile(mobile)){
+            return RespBean.error(RespBeanEnum.MOBILE_ERROR);
+        }
+        //根据手机号获取用户
+        User user = userMapper.selectById(mobile);
+        if (user == null){
+            return  RespBean.error(RespBeanEnum.LOGIN_ERROR);
+        }
+        //判断密码是否正确
+        if (!MD5Util.fromPassToDBPass(password, user.getSalt()).equals(user.getPassword())){
+            return  RespBean.error(RespBeanEnum.LOGIN_ERROR);
+        }
+
+        return RespBean.success(RespBeanEnum.SUCCESS);
+    }
+
+}
