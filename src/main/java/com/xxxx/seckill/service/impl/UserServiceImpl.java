12a2407cee5a263573fa325b5af219663ce12510
yanshanbei <1143219505@qq.com>
2023-09-14 19:31:17 +0800

    将登录页的error用全局异常抛出

--- src/main/java/com/xxxx/seckill/service/impl/UserServiceImpl.java
+++ src/main/java/com/xxxx/seckill/service/impl/UserServiceImpl.java
@@ -1,20 +1,17 @@
 package com.xxxx.seckill.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xxxx.seckill.exception.GlobalException;
 import com.xxxx.seckill.mapper.UserMapper;
 import com.xxxx.seckill.pojo.User;
 import com.xxxx.seckill.service.IUserService;
 import com.xxxx.seckill.utils.MD5Util;
-import com.xxxx.seckill.utils.ValidatorUtil;
 import com.xxxx.seckill.vo.LoginVo;
 import com.xxxx.seckill.vo.RespBean;
 import com.xxxx.seckill.vo.RespBeanEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -42,20 +39,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         String mobile = loginVo.getMobile();
         String password = loginVo.getPassword();
 
-        if (StringUtils.isEmpty(mobile) || StringUtils.isEmpty(password)){
-            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
-        }
-        if (!ValidatorUtil.isMobile(mobile)){
-            return RespBean.error(RespBeanEnum.MOBILE_ERROR);
-        }
+//        if (StringUtils.isEmpty(mobile) || StringUtils.isEmpty(password)){
+//            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
+//        }
+//        if (!ValidatorUtil.isMobile(mobile)){
+//            return RespBean.error(RespBeanEnum.MOBILE_ERROR);
+//        }
         //根据手机号获取用户
         User user = userMapper.selectById(mobile);
         if (user == null){
-            return  RespBean.error(RespBeanEnum.LOGIN_ERROR);
+            throw new GlobalException(RespBeanEnum.LOGIN_ERROR);
         }
         //判断密码是否正确
         if (!MD5Util.fromPassToDBPass(password, user.getSalt()).equals(user.getPassword())){
-            return  RespBean.error(RespBeanEnum.LOGIN_ERROR);
+            throw new GlobalException(RespBeanEnum.LOGIN_ERROR);
         }
 
         return RespBean.success(RespBeanEnum.SUCCESS);
