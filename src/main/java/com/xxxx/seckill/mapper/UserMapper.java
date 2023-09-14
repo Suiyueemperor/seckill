99a888fb3ec4c34854033ea73ad4f2701672823e
yanshanbei <1143219505@qq.com>
2023-09-14 18:34:30 +0800

    根据t_user表逆向生成的

---
+++ src/main/java/com/xxxx/seckill/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.xxxx.seckill.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xxxx.seckill.pojo.User;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Mr.Shi
+ * @since 2023-09-03
+ */
+public interface UserMapper extends BaseMapper<User> {//建立数据库与业务的映射, 常在DAO或DAL
+
+}
