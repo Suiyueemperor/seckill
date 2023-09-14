6f0743f17930626552734d9d912e923ec8d8f59f
yanshanbei <1143219505@qq.com>
2023-09-14 19:50:58 +0800

    add goodsList, userController
    修改login.html, 登录后跳转到goods/toList

---
+++ src/main/java/com/xxxx/seckill/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.xxxx.seckill.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Mr.Shi
+ * @since 2023-09-03
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
