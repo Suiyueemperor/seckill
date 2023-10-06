d2b549c940583be26cfb11cbccaa61e7366fee8e
yanshanbei <1143219505@qq.com>
2023-10-06 21:48:32 +0800

    处理订单详情

--- src/main/java/com/xxxx/seckill/controller/OrderController.java
+++ src/main/java/com/xxxx/seckill/controller/OrderController.java
@@ -1,6 +1,13 @@
 package com.xxxx.seckill.controller;
 
+import com.xxxx.seckill.pojo.User;
+import com.xxxx.seckill.service.IOrderService;
+import com.xxxx.seckill.vo.OrderDetailVo;
+import com.xxxx.seckill.vo.RespBean;
+import com.xxxx.seckill.vo.RespBeanEnum;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private IOrderService orderService;
+
+    /**
+     * 订单详情
+     * @param user
+     * @param orderId
+     * @return
+     */
+    @RequestMapping("/detail")
+    @ResponseBody
+    public RespBean detail(User user, Long orderId){
+        if (user == null) {
+            return RespBean.error(RespBeanEnum.SEESION_ERROR);
+        }
+        OrderDetailVo detail = orderService.detail(orderId);
+        return RespBean.success(detail);
+    }
 
 }
