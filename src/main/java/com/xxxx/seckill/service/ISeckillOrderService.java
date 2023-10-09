a2004d3d091f72f2de31f9410f1a7db44b99eb10
yanshanbei <1143219505@qq.com>
2023-10-09 17:58:23 +0800

    获取秒杀结果

--- src/main/java/com/xxxx/seckill/service/ISeckillOrderService.java
+++ src/main/java/com/xxxx/seckill/service/ISeckillOrderService.java
@@ -2,6 +2,7 @@ package com.xxxx.seckill.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.seckill.pojo.SeckillOrder;
+import com.xxxx.seckill.pojo.User;
 
 /**
  * <p>
@@ -13,4 +14,12 @@ import com.xxxx.seckill.pojo.SeckillOrder;
  */
 public interface ISeckillOrderService extends IService<SeckillOrder> {
 
+    /**
+     * 获取秒杀结果
+     * @param user
+     * @param goodsId
+     * @return
+     * orderId 成功; -1:失败,0:排队中
+     */
+    Long getResult(User user, Long goodsId);
 }
